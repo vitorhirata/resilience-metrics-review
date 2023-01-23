b43f03082f43b4cb5f1aa3d465cf767341fc82b9
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2023-01-23 20:43:36 -0300

    Update cleaning file acording to methodology design change
    The literature review will now be divided in two searches, it update the
    cleaning file to adequate to this change. It also updates file names
    to reflect the change in folder structure.

--- cleaning.py
+++ cleaning.py
@@ -1,23 +1,30 @@
 import pandas as pd
+from os.path import exists
 
-df1Full = pd.read_csv('tmp/1_full.csv', nrows=5)
-column_names = list(df1Full.columns)[:-1]
+dfNames = pd.read_csv('bulk_data/column_names.csv', nrows=5)
+column_names = list(dfNames.columns)[:-1]
 
-df1 = pd.read_csv('tmp/1.csv', delimiter='\t', header=0, names=column_names)
+for search_name in ['review', 'model']:
 
-delete_columns = []
-for name, values in df1.iteritems():
-  if values.isna().sum() == 1000:
-    delete_columns.append(name)
-print("Dropped columns", delete_columns)
+  df1 = pd.read_csv(f'bulk_data/{search_name}_1.csv', delimiter='\t', header=0, names=column_names)
 
-df1 = df1.drop(delete_columns, axis=1)
+  delete_columns = []
+  for name, values in df1.iteritems():
+    if values.isna().sum() == 1000:
+      delete_columns.append(name)
+  print("Dropped columns", delete_columns)
 
-for i in range(2,13):
-  df2 = pd.read_csv(f"tmp/{i}.csv", delimiter='\t', header=0, names=column_names)
-  df2 = df2.drop(delete_columns, axis=1)
-  df1 = df1.append(df2)
+  df1 = df1.drop(delete_columns, axis=1)
 
-print("Final dataframe information", df1.info())
+  for i in range(2,13):
+    df2 = pd.read_csv(f"bulk_data/{search_name}_{i}.csv", delimiter='\t', header=0, names=column_names)
+    df2 = df2.drop(delete_columns, axis=1)
+    df1 = df1.append(df2)
 
-df1.to_csv('references.csv')
+    if not exists(f"bulk_data/{search_name}_{i + 1}.csv"): break
+
+  print(f"Final dataframe information {search_name}", df1.info())
+
+  df1.to_csv(f'data/{search_name}.csv')
+
+print("The end.")
